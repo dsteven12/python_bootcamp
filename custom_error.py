98530ec3f5928565ab34ab1d13b4f81b7791e7ee
Darryl Lardizabal <dsteven12@gmail.com>
2021-02-08 18:55:17 -0800

    Feature: Added custom error message using Type Error as Parent Class.

--- custom_error.py
+++ custom_error.py
@@ -1,4 +1,11 @@
-class CustomError(TypeError):
-    pass
+class RuntimeErrorWithCode(TypeError): #If not a TypeError, can use a base error like 'Exception'
+    """
+    Exception raised when a specific error code is needed
+    """
+    def __init__(self, message, code):
+        super().__init__(f"Error code {code}: {message}")
+        self.code = code
 
-raise CustomError("Ouch! An error happened.")
+err = RuntimeErrorWithCode("Ouch! An error happened.", 400)
+print(f"Checking doc string of RuntimeErrorWithCode: {err.__doc__}")
+raise err
